cf160ae1f1af1b05cd72ce5802cf4b09acb308d8
Yukuro <tupledev9@gmail.com>
2019-03-07 20:11:19 +0900

    デバッグ用処理を最適化

--- MKRWAN1300_test2/src/main.cpp
+++ MKRWAN1300_test2/src/main.cpp
@@ -21,12 +21,12 @@ void blinkLED(const int LEDPIN, int time);
 loraFrequency carrierSense();
 
 void setup() {
-  Serial.println("[DEBUG] setup start");
-  pinMode(DEBUGLED_PIN,OUTPUT);
-  digitalWrite(DEBUGLED_PIN, LOW);
-  
   Serial.begin(115200);
   Serial.setTimeout(3000);
+
+  pinMode(DEBUGLED_PIN,OUTPUT);
+  digitalWrite(DEBUGLED_PIN, LOW);
+
   while (!Serial);
   if(!lora9216e5.begin(9216E5)){
     while(1) blinkLED(DEBUGLED_PIN, 100);
@@ -43,59 +43,71 @@ void setup() {
   lora9216e5.setTxPower(13);
   lora9218e5.setTxPower(13);
   lora9220e5.setTxPower(13);
+
+  //注意書き
+  Serial.println("DO NOT USE ENTER KEY"); // TODO : CRが扱える処理に変更
 }
 
 void loop() {
-  Serial.println("[DEBUG] loop start");
+  //Serial.println("[DEBUG] loop start");
+  g_commflag = true; //デフォルトでは送信許可
 
   g_Freq = carrierSense();
-  if(g_Freq != LORASTAND) g_commflag = false; //キャリアセンス失敗時は送信禁止
+  if(g_Freq == LORASTAND) g_commflag = false; //キャリアセンス失敗時は送信禁止
   switch(g_Freq){
     case LORA9216E5:
     {
-      String sendcommand;
-      if(Serial.available() > 0){
-        sendcommand = Serial.readStringUntil('\n');
-        if(sendcommand.indexOf("[DEBUG]") != -1) g_commflag = false;
-      }
-      if(g_commflag){
-        lora9216e5.beginPacket();
-        lora9216e5.print(sendcommand);
-        lora9216e5.print('\n');
-        lora9216e5.endPacket();
+      Serial.println("[INPUT:3s] command at 921.6MHz : ");
+      String sendcommand = Serial.readStringUntil('\n'); //シリアルバッファから文字列受け取り
+      if(sendcommand.length() > 0){
+        Serial.print("[DEBUG] command is "); 
+        Serial.print(sendcommand);
+        Serial.print(" ... ");
+
+        if(g_commflag){
+          lora9216e5.beginPacket();
+          lora9216e5.print(sendcommand);
+          lora9216e5.print('\n');
+          Serial.println("sended");
+          lora9216e5.endPacket();
+        }
       }
       break;
     }
     
     case LORA9218E5:
     {
-      String sendcommand;
-      if(Serial.available() > 0){
-        sendcommand = Serial.readStringUntil('\n');
-        if(sendcommand.indexOf("[DEBUG]") != -1) g_commflag = false;
-      }
-      if(g_commflag){
-      lora9218e5.beginPacket();
-      lora9218e5.print(sendcommand);
-      lora9218e5.print('\n');
-      lora9218e5.endPacket();
+      Serial.println("[INPUT:3s] command at 921.8MHz : ");
+      String sendcommand = Serial.readStringUntil('\n');
+      if(sendcommand.length() > 0){
+        Serial.print("[DEBUG] command is "); Serial.print(sendcommand); Serial.print("... ");
+
+        if(g_commflag){
+          lora9218e5.beginPacket();
+          lora9218e5.print(sendcommand);
+          lora9218e5.print('\n');
+          Serial.println("sended");
+          lora9218e5.endPacket();
+        }
       }
       break;
     }
 
     case LORA9220E5:
     {
-      String sendcommand;
-      if(Serial.available() > 0){
-        sendcommand = Serial.readStringUntil('\n');
-        if(sendcommand.indexOf("[DEBUG]") != -1) g_commflag = false;
+      Serial.println("[INPUT:3s] command at 922.0MHz : ");
+      String sendcommand = Serial.readStringUntil('\n');
+      if(sendcommand.length() > 0){
+        Serial.print("[DEBUG] command is "); Serial.print(sendcommand); Serial.print("... ");
+
+        if(g_commflag){
+          lora9220e5.beginPacket();
+          lora9220e5.print(sendcommand);
+          lora9220e5.print('\n');
+          Serial.println("sended");
+          lora9220e5.endPacket();
+        }
       }
-      if(g_commflag){
-        lora9220e5.beginPacket();
-        lora9220e5.print(sendcommand);
-        lora9220e5.print('\n');
-        lora9220e5.endPacket();
-      } 
       break;
     }
 
@@ -128,36 +140,48 @@ loraFrequency carrierSense(){
   int packetSize;
 
   while(entrytime - starttime <= 128){
-  packetSize = lora9216e5.parsePacket();
-    if(packetSize){
-      int rssi = lora9216e5.packetRssi(); //RSSIを取得
-      if(rssi <= -80) csallow = true; //-80dBm以上の出力があった場合には電波の放出を禁止
-    }
-    entrytime = micros();
+    packetSize = lora9216e5.parsePacket();
+      if(packetSize){
+        Serial.print("[NOTICE] : Entry CS(921.6MHz) at RSSI = ");
+        int rssi = lora9216e5.packetRssi(); //RSSIを取得
+        Serial.println(rssi);
+        if(rssi <= -80) csallow = true; //-80dBm以下の出力なら電波の放出を許可
+      }else{
+        csallow = true; //その周波数帯が使われていないなら電波の放出を許可
+      }
+      entrytime = micros();
   }
   if(csallow) return LORA9216E5;
 
   starttime = micros();
   entrytime = micros();
   while(entrytime - starttime <= 128){
-  packetSize = lora9218e5.parsePacket();
-    if(packetSize){
-      int rssi = lora9218e5.packetRssi();
-      if(rssi <= -80) csallow = true;
-    }
-    entrytime = micros();
+    packetSize = lora9218e5.parsePacket();
+      if(packetSize){
+        Serial.print("[NOTICE] : Entry CS(921.8MHz) at RSSI = ");
+        int rssi = lora9218e5.packetRssi();
+        Serial.println(rssi);
+        if(rssi <= -80) csallow = true;
+      }else{
+        csallow = true;
+      }
+      entrytime = micros();
   }
   if(csallow) return LORA9218E5;
 
   starttime = micros();
   entrytime = micros();
   while(entrytime - starttime <= 128){
-  packetSize = lora9220e5.parsePacket();
-    if(packetSize){
-      int rssi = lora9220e5.packetRssi();
-      if(rssi <= -80) csallow = true;
-    }
-    entrytime = micros();
+    packetSize = lora9220e5.parsePacket();
+      if(packetSize){
+        Serial.print("[NOTICE] : Entry CS(922.0MHz) at RSSI = ");
+        int rssi = lora9220e5.packetRssi();
+        Serial.println(rssi);
+        if(rssi <= -80) csallow = true;
+      }else{
+        csallow = true;
+      }
+      entrytime = micros();
   }
   if(csallow) return LORA9220E5;
 
